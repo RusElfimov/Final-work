0daa82f94a2a29fa074870d8edad77d7a2ade957
RusElfimov <ruselfimovbox@outlook.com>
2022-12-01 11:43:28 +0300

    create

---
+++ work/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+void SortArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<=3)
+        Console.Write (array[i] + " ");
+        else
+        Console.Write (" ");
+    }
+}
+
+string [] array = {"1234", "76", "-22","58673", "3555", "24234"};
+SortArray (array);
